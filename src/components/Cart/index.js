090985aebfb41a914c3c5f391bc579334fa36521
Anjana19JK <anajnajoseph199@gmail.com>
2023-01-05 06:27:26 +0000

    logout

---
+++ src/components/Cart/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+      />
+      <h1>Cart</h1>
+    </div>
+  </>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+      <h1>Products</h1>
+    </div>
+  </>
+)
+
+export default Products
